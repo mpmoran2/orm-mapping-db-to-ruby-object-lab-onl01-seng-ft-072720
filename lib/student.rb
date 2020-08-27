35fe0ba81a5b6758cac65071baa53a8a44764314
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-27 17:59:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,14 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      student = self.new_from_db(row)
+    end
   end 
   
   def self.students_below_12th_grade
@@ -31,9 +39,8 @@ class Student
   def self.first_student_in_grade_10
   end 
   
-  def self
+  def self.all_students_in_grade_X
   end 
-  
   
   def self.find_by_name(name)
     sql = <<-SQL
