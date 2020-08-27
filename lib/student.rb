fce6fcad232ee741688d314f1a90d6819b3deeab
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-27 18:03:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,14 +44,14 @@ class Student
   def self.first_X_students_in_grade_10(number)
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 10 
+      WHERE grade = 10
       LIMIT ?
     SQL
-    
-    DB[:conn].execute(sql, name).collect do |row|
+
+    DB[:conn].execute(sql, number).map do |row|
       student = self.new_from_db(row)
     end
-  end 
+  end
   
   def self.first_student_in_grade_10
     sql = <<-SQL
